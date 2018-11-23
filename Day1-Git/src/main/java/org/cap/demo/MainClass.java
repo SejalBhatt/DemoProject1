50a75f0f3f9e84f7de9a2ff2c0bf7ddf7455089a
sebhatt <sebhatt@DIN73002599.corp.capgemini.com>
2018-11-23 14:31:54 +0530

    project committed

---
+++ Day1-Git/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello All!");
+		greetings();
+	}
+	
+	public static void greetings()	{
+		System.out.println("Welcome to Java");
+	}
+
+}
